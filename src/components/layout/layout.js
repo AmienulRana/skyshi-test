0f31daba115c5d60f7b98c1b32f03aa9b0ece3ad
AmienulRana <amienulrana@gmail.com>
2022-08-10 10:41:10 +0700

    memperbaiki header title agar terbaca oleh unit test

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 export default function Layout({ children }){
     return(
         <div className={styles.layout}>
-            <nav data-cy="header-background">
-                <h1 data-cy="header-title">TO DO LIST APP</h1>
+            <nav data-cy="header">
+                <div data-cy="header-background" className={styles.headerBakcgorund}>
+                    <h1 data-cy="header-title">TO DO LIST APP</h1>
+                </div>
             </nav>
             <main>
                 { children }
